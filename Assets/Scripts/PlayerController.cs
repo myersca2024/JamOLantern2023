4894e64efb8281cd78656b111bec5134dfe08494
myersca2024 <myers.ca@northeastern.edu>
2023-10-21 21:33:06 -0400

    Added simple player movement
    Also includes new Unity Input System package

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+/// <summary>
+/// Simple player controller using new Unity Input System.
+/// </summary>
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private float movementSpeed;
+
+    private PlayerControls playerControls;
+
+    private void Awake()
+    {
+        playerControls = new PlayerControls();
+
+        playerControls.Player.Enable();
+    }
+
+    private void Update()
+    {
+        MovePlayer();
+    }
+
+    /// <summary>
+    /// Moves player by reading the "Movement" Vector2 in PlayerControls.
+    /// Called every frame.
+    /// </summary>
+    private void MovePlayer()
+    {
+        Vector2 inputVector = playerControls.Player.Movement.ReadValue<Vector2>();
+        Vector3 movementVector = new Vector3(inputVector.x, 0f, inputVector.y);
+        transform.position += movementVector * movementSpeed * Time.deltaTime;
+    }
+}
