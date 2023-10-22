bb93b8dfd860489fc61b3881027e636181889d10
myersca2024 <myers.ca@northeastern.edu>
2023-10-21 22:29:36 -0400

    Added sprite billboarding + comment tidying

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 /// <summary>
 /// Simple player controller using new Unity Input System.
